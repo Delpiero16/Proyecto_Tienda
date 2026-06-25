6b87d2c4347b00990bd94fa4f274db05fbe5fa32
brayan vilcahuaman <brayanjosephgamer@gamail.com>
2026-06-25 06:58:36 +0200

    Subir sistema de inventario

---
+++ python.py
@@ -0,0 +1,24 @@
+import os
+
+# ==========================
+# ARCHIVO
+# ==========================
+
+ARCHIVO = "inventario.txt"
+
+# ==========================
+# LISTAS
+# ==========================
+
+categorias = []
+nombres_productos = []
+stock_productos = []
+precios_productos = []
+
+historial_ventas = []
+historial_cantidades = []
+historial_totales = []
+
+MAX_PRODUCTOS = 50
+MAX_STOCK = 36
+
