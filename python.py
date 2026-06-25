cdeeed885fcb3cb1a9b42b5fc027ae29c02c12be
brayan vilcahuaman <brayanjosephgamer@gamail.com>
2026-06-25 07:19:06 +0200

    cargar archivo

--- python.py
+++ python.py
@@ -2,12 +2,18 @@ import os
 
 # ==========================
 # ARCHIVO
+# Variable donde se guarda el nombre del archivo de texto.
+# En este archivo se almacenan los productos para no perderlos al cerrar el programa.
 # ==========================
 
 ARCHIVO = "inventario.txt"
 
 # ==========================
 # LISTAS
+# listas paralelas del inventario.
+# cada posicion representa un mismo producto.
+# ejemplo: categorias[0] nombres_productos[0] y asi los demas 
+# pertenecen al mismo producto.
 # ==========================
 
 categorias = []
@@ -21,4 +27,36 @@ historial_totales = []
 
 MAX_PRODUCTOS = 50
 MAX_STOCK = 36
+
+# ==========================
+# CARGAR ARCHIVO
+# Sirve para leer los productos guardados en el invetario.txt
+# Se ejecuta al iniciar el programa para recuperar la informacion anterior.
+# Si el archivo no existe, no carga nada y el programa empieza vacio.
+# ==========================
+
+def cargar_archivo():
+
+  if not os.path.exists(ARCHIVO):
+    return
+
+  with open(ARCHIVO, "r", encoding="utf-8") as archivo:
+    for linea in archivo:
+
+      datos = linea.strip().split(",")
+
+      if len(datos) == 3:
+
+        categorias.append(datos[0])
+        nombres_productos.append(datos[1])
+        stock_productos.append(int(datos[2]))
+        precios_productos.append(0)
+
+      elif len(datos) == 4:
+
+        categorias.append(datos[0])
+        nombres_productos.append(datos[1])
+        stock_productos.append(int(datos[2]))
+        precios_productos.append(float(datos[3]))
+
 
